9913396aa95e294df06effee4c0a47ec4a94aec5
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-20 16:45:25 +0300

    Delete Задача 6.py

--- Задача 6.py
+++
@@ -1,9 +0,0 @@
-from random import random, randint
-numbers1 = {1, 2, 3, 4, 5}
-numbers2 = {5, 6, 7, 8, 1}
-print(numbers1)
-print(numbers2)
-
-
-print('Повторяющиеся элементы в двух множествах:')
-print(numbers1 & numbers2)
