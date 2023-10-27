d090f130b1d98cf7ef3014f34c4ac4465aa07d74
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-27 18:03:47 +0300

    Update Задание 4.py

--- Задание 4.py
+++ Задание 4.py
@@ -3,16 +3,18 @@ final_list = []
 list_of_firms = []
 dict_of_firms = {}
 profit = 0
+kol = 0
 with open("Firms.txt") as firms:
     for read_line in firms:
         list_of_firms.append(read_line.split())
 for firm in list_of_firms:
     if len(firm) == 4:
-        dict_of_firms[firm[0]] = firm[3]
+        dict_of_firms[firm[0]] = '-' + firm[3]
+        kol += 1
     else:
         dict_of_firms[firm[0]] = firm[2]
         profit += int(firm[2])
-average_profit = profit/int(len(list_of_firms))
+average_profit = profit/int(len(list_of_firms) - kol)
 profit_dict = {"average_profit": average_profit}
 final_list = [dict_of_firms, profit_dict]
 print(final_list)
